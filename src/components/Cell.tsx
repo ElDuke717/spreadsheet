3bc141daf902534e384bddac53bac33d872b594a
elduke717 <spacematcha@proton.me>
2023-12-05 10:43:07 -0500

    add select and unselect cell capability

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,7 +1,14 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+
 
 export default function Cell() {
   const [selected, setSelected] = useState(false);
+
+  useEffect( () => {
+  window.document.addEventListener('unselectAll', () => {
+    setSelected(false);
+  })
+}, []);
 
   if (selected) {
     return <input className="border-2 p-3 bg-blue-500" />;
@@ -11,6 +18,7 @@ export default function Cell() {
     <div
       className="border-2 p-3"
       onClick={() => {
+        window.document.dispatchEvent(new Event('unselectAll'));
         setSelected(true);
       }}
     >
