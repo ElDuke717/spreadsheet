38085c28a570cdde31cc0888fd8328a6dc334eb6
elduke717 <spacematcha@proton.me>
2023-12-05 10:36:32 -0500

    add ability to select cell

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,3 +1,20 @@
+import { useState } from "react";
+
 export default function Cell() {
-  return <div className="border-2 p-3">stuff</div>;
+  const [selected, setSelected] = useState(false);
+
+  if (selected) {
+    return <input className="border-2 p-3 bg-blue-500" />;
+  }
+
+  return (
+    <div
+      className="border-2 p-3"
+      onClick={() => {
+        setSelected(true);
+      }}
+    >
+      cell
+    </div>
+  );
 }
