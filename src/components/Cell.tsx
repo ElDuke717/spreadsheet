b0eebc7215ca9bcee54d8127186d1357721dd391
elduke717 <spacematcha@proton.me>
2023-12-05 11:34:57 -0500

    add comments

--- src/components/Cell.tsx
+++ src/components/Cell.tsx
@@ -1,41 +1,53 @@
+// Import useState and useEffect hooks from React
 import { useState, useEffect } from "react";
 
+// Define the type for props that this component will accept
 type Props = {
-  initialValue: string;
-  x: number;
-  y: number;
-  updateValue: (x: number, y: number, value: string) => void;
+  initialValue: string; // initial value for the cell
+  x: number; // x-coordinate (or row index)
+  y: number; // y-coordinate (or column index)
+  updateValue: (x: number, y: number, value: string) => void; // function to update the cell value
 };
 
+// Define the Cell component
 export default function Cell({ value, x, y, updateValue }: Props) {
+  // State for tracking whether the cell is selected or not
   const [selected, setSelected] = useState(false);
 
+  // useEffect hook to add an event listener to the window object
   useEffect(() => {
+    // Add a listener for the 'unselectAll' event
     window.document.addEventListener("unselectAll", () => {
+      // Set 'selected' state to false when 'unselectAll' event is dispatched
       setSelected(false);
     });
-  }, []);
+  }, []); // Empty dependency array means this effect runs only once after the component mounts
 
+  // If the cell is selected, return an input element
   if (selected) {
     return (
       <input
-        className="border-2 p-3 bg-blue-200"
-        value={value}
+        className="border-2 p-3 bg-blue-200" // Styling classes for the input
+        value={value} // Set the input value to the value passed in props
         onChange={(e) => {
+          // Update the cell value on change
           updateValue(x, y, e.target.value);
         }}
       />
     );
   }
 
+  // Default return when the cell is not selected
   return (
     <div
-      className="border-2 p-3"
+      className="border-2 p-3" // Styling classes for the div
       onClick={() => {
+        // Dispatch 'unselectAll' event and set the current cell as selected on click
         window.document.dispatchEvent(new Event("unselectAll"));
         setSelected(true);
       }}
     >
+      {/* Display the cell value or a placeholder if the value is empty */}
       {value || <span className="text-gray-200">-</span>}
     </div>
   );
--- src/components/Spreadsheet.tsx
+++ src/components/Spreadsheet.tsx
@@ -21,40 +21,46 @@ export default function Spreadsheet() {
     ['', '', '', '', '', '', '', '', '', ''],
   ]);
 
+  // Function to update the value of a cell in the spreadsheet.
   function updateValue(x: number, y: number, value: string) {
+    // Creating a deep clone of the current 'data' state to avoid direct mutation.
     const _data = structuredClone(data);
+    // Updating the specified cell with the new value.
     _data[y][x] = value;
+    // Updating the 'data' state with the new array.
     setData(_data);
 
+    // Storing the updated data in the browser's local storage if available.
     if (window && window.localStorage) {
       window.localStorage.setItem('spreadsheet', JSON.stringify(_data))
     }
   }
 
+  // useEffect hook to retrieve and set the spreadsheet data from local storage when the component mounts.
   useEffect(() => {
     const data = window.localStorage.getItem('spreadsheet')
     if (data) setData(JSON.parse(data))
-  }, [])
+  }, []) // Empty dependency array ensures this effect runs only once after the component mounts.
 
   // Rendering the Spreadsheet component.
-  /* possible grid-cols-* values are grid-cols-1 grid-cols-2 grid-cols-3 grid-cols-4 grid-cols-5 grid-cols-6 grid-cols-7 grid-cols-8 grid-cols-9 grid-cols-10 */
   return (
+    // Using a grid layout with a number of columns equal to the length of the first row in 'data'.
     <div className={`grid grid-cols-${data[0].length}`}>
-      {/* Mapping over each row in the data array. 'row' represents a single row, and 'y' is its index. */}
-      {data.map((row, y) => {
-        // For each row, map over each cell in the row.
-        return row.map((cell, x) => {
-          // Rendering the Cell component for each cell in the row.
-          // Assigning a unique key using the row and column indices to help React identify which items have changed.
-          return <Cell 
-            key={y + '-' + x}
-            value = {cell} 
-            x = {x}
+      {/* Mapping over each row in the 'data' array. 'row' is a single row, and 'y' is its index. */}
+      {data.map((row, y) => (
+        // For each row, mapping over each cell.
+        row.map((cell, x) => (
+          // Rendering a Cell component for each cell.
+          // The 'key' prop helps React track which items have changed, are added, or are removed.
+          <Cell 
+            key={`${y}-${x}`}
+            value={cell} 
+            x={x}
             y={y}
-            updateValue = {updateValue}
+            updateValue={updateValue}
           />
-        })
-      })}
+        ))
+      ))}
     </div>
   )
 }
